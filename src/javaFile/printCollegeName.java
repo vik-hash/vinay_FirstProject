5337b1417777245377282159e4f96d72651e05f8
vikas-stack <vikash7044@outlook.com>
2020-01-12 21:55:32 +0530

    my first commit in vinay

---
+++ src/javaFile/printCollegeName.java
@@ -0,0 +1,9 @@
+package javaFile;
+
+public class printCollegeName {
+public static void main(String[] args) {
+System.out.println("vishweshwarya institute of engineering and technology");	
+}
+
+}
+
